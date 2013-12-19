3de11287b0fa10ba216600c3ac1c0bdc931c576f
Sya <sya.bousquet@gmail.com>
2013-12-19 09:39:51 +0100

    Version finale

--- clientWebService/googlePlus.php
+++ clientWebService/googlePlus.php
@@ -1,103 +1,102 @@
-<!DOCTYPE html>  
-<html lang="fr">  
-	<head>  
-    <meta charset="utf-8">
-	  <title>Mise en oeuvre de l’API Google+ en PHP : Partie 1 – Afficher votre profil</title> 
-	  
-	  <!-- include -->
-    <link rel="stylesheet" href="../style.css" />	
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script> 
-    
-    <style>
-    	.Ec {
-		    color: #888888;
-		    font-size: 15px;
-		    font-weight: normal;
-		    line-height: 1.2em;
-		    margin-bottom: 0;
-		    text-align: right;
-		    vertical-align: top;
-		    width: 110px;
-			}
-			.a-l-k {
-			    display: inline-block;
-			    position: relative;
-			}
-			h1, h2 {
-			    border: 0 none;
-			    margin: 0;
-			}
-			.Aa {
-			    line-height: 1.39em;
-			    padding: 0 0 0 15px;
-			    vertical-align: top;
-			    width: 800px;
-			}
-			.a-l-k {
-			    display: inline-block;
-			    position: relative;
-			}
-			.ts {
-			    border: medium none;
-			}
-			.Ic {
-			    color: #333333;
-			    font: 13px arial,sans-serif;
-			    margin: 0;
-			    padding: 10px 0;
-			    text-align: left;
-			}
-			em { 
-				color: #999999; 
-			}
-    </style> 
-	</head> 
-	 
-	<body>  
-		<div class="center">
-			<?php
-				$profil_googleplus_json = file_get_contents("https://www.googleapis.com/plus/v1/people/116475369142621368056?key=3Al35hLUAROhyoqiqbCzzVWY");
-				$profil_googleplus = json_decode($profil_googleplus_json);
-			?>	
-			<div class="bio">
-				<div class="g-Jd ts Ic pb" style="text-align: center">
-					<img src="<?php echo $profil_googleplus->image->url; ?>" alt="Photo de profil" />
-				</div>
-				
-				<div class="g-Jd ts Ic pb">
-					<h2 class="Ec a-l-k">Nom</h2>
-					<div class="Aa a-l-k note"><?php echo $profil_googleplus->displayName; ?> <em>(<?php echo $profil_googleplus->tagline; ?>)</em></div>
-				</div>
-				
-				<div class="g-Jd ts Ic pb">
-				  <h2 class="Ec a-l-k">Présentation</h2>
-				  <div class="Aa a-l-k note"><?php echo $profil_googleplus->aboutMe; ?></div>
-				</div>
-				
-				<div class="g-Jd ts Ic pb">
-					<h2 class="Ec a-l-k">Emplois</h2>
-					<?php foreach ($profil_googleplus->organizations as $_organization) { 
-									if ($_organization->type != "work") { 
-										continue;
-									}
-					?>
-					<div class="Aa a-l-k note"><?php echo $_organization->name; ?> <em>(<?php echo $_organization->title; ?>)</em></div>
-					<?php } ?>
-				</div>
-				
-				<div class="g-Jd ts Ic pb">
-					<h2 class="Ec a-l-k">Formation</h2>
-					<?php foreach ($profil_googleplus->organizations as $_organization) { 
-									if ($_organization->type != "school") { 
-										continue;
-									}
-					?>
-					<div class="Aa a-l-k note"><?php echo $_organization->name; ?> <em>(<?php echo $_organization->title; ?>)</em></div>
-					<?php } ?>
-				</div>
-			</div>
-			
-	  </div>
-			
-  </body>  
-</html>  
+<?php
+/*
+ * Copyright 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+require_once 'google-api-php-client/src/Google_Client.php';
+require_once 'google-api-php-client/src/contrib/Google_PlusService.php';
+
+session_start();
+
+$client = new Google_Client();
+$client->setApplicationName("WebServiceTP");
+//$url="https://www.googleapis.com/plus/v1/people/116475369142621368056?key=3Al35hLUAROhyoqiqbCzzVWY";
+// Visit https://code.google.com/apis/console to generate your
+// oauth2_client_id, oauth2_client_secret, and to register your oauth2_redirect_uri.
+$client->setClientId('571568646192-5dl6t04gtrlrdisfiu9ih4astem8st91.apps.googleusercontent.com');
+$client->setClientSecret('OpmfbKpckkDN9phLJzTlvRjN'); 
+$client->setRedirectUri('http://localhost/clientWebService/googlePlus.php'); 
+$client->setDeveloperKey('AIzaSyAUNeOi4vPHnNz_QIElLuhIL9e5I7A8Nu0'); //API Key
+$plus = new Google_PlusService($client);
+
+if (isset($_REQUEST['logout'])) {
+  unset($_SESSION['access_token']);
+}
+
+if (isset($_GET['code'])) {
+  $client->authenticate($_GET['code']);
+  $_SESSION['access_token'] = $client->getAccessToken();
+  header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']);
+}
+
+if (isset($_SESSION['access_token'])) {
+  $client->setAccessToken($_SESSION['access_token']);
+}
+
+if ($client->getAccessToken()) {
+  $me = $plus->people->get('me');
+
+  // These fields are currently filtered through the PHP sanitize filters.
+  // See http://www.php.net/manual/en/filter.filters.sanitize.php
+  $url = filter_var($me['url'], FILTER_VALIDATE_URL);
+  $img = filter_var($me['image']['url'], FILTER_VALIDATE_URL);
+  $name = filter_var($me['displayName'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+  $personMarkup = "<a rel='me' href='$url'>$name</a><div><img src='$img'></div>";
+
+  $optParams = array('maxResults' => 100);
+  $activities = $plus->activities->listActivities('me', 'public', $optParams);
+  $activityMarkup = '';
+  foreach($activities['items'] as $activity) {
+    // These fields are currently filtered through the PHP sanitize filters.
+    // See http://www.php.net/manual/en/filter.filters.sanitize.php
+    $url = filter_var($activity['url'], FILTER_VALIDATE_URL);
+    $title = filter_var($activity['title'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+    $content = filter_var($activity['object']['content'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+    $activityMarkup .= "<div class='activity'><a href='$url'>$title</a><div>$content</div></div>";
+  }
+
+  // The access token may have been updated lazily.
+  $_SESSION['access_token'] = $client->getAccessToken();
+} else {
+  $authUrl = $client->createAuthUrl();
+}
+?>
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <link rel='stylesheet' href='style.css' />
+</head>
+<body>
+<header><h1>Google API</h1></header>
+<div class="box">
+
+<?php if(isset($personMarkup)): ?>
+<div class="me"><?php print $personMarkup ?></div>
+<?php endif ?>
+
+<?php if(isset($activityMarkup)): ?>
+<div class="activities">Your Activities: <?php print $activityMarkup ?></div>
+<?php endif ?>
+
+<?php
+  if(isset($authUrl)) {
+    print "<a class='login' href='$authUrl'>Connect Me!</a>";
+  } else {
+   print "<a class='logout' href='?logout'>Logout</a>";
+  }
+?>
+</div>
+</body>
+</html>
